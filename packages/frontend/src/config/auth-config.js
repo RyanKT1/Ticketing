2be3842a315fcb9ec2a761b0fbdf155d7cafbd9b
Ryan Kenfack Tsafack <ryakenfa@amazon.com>
2025-07-08 13:02:21 +0100

    Fix Cognito incorrect user pool

--- packages/frontend/src/config/auth-config.js
+++ packages/frontend/src/config/auth-config.js
@@ -1,6 +1,6 @@
 // Add these after cdk deployment
 export const authConfig = {
-  authority: "https://cognito-idp.eu-west-2.amazonaws.com/eu-west-2_eu-west-2_mCkMgiuT8",
+  authority: "https://cognito-idp.eu-west-2.amazonaws.com/eu-west-2_mCkMgiuT8",
   client_id: "4ljm4p4e4il0me3ocuas5d9r3n",
   redirect_uri: window.location.origin,
   response_type: "code",
